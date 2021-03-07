9782cafefc85d705ccb8f6edacb6eb50e499afd7
Claire <claire.brisbart@edu.devinci.fr>
2021-03-07 22:45:23 +0100

    ADD - components ArticleList & CreateArticle

---
+++ src/components/ArticleList.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="article-list">
+    <img :src="img" alt="image de base">
+    <p>{{ text }}</p>
+    <button><i class="fas fa-edit"></i></button>
+    <button><i class="fas fa-trash"></i></button>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    name: 'article-list',
+    computed: {
+        ...mapState(['img', 'text'])
+    }
+}
+</script>
+
+<style>
+#article-list {
+    display: flex;
+    align-items: center;
+    width: 80%;
+    margin: 0 auto 3vh;
+    padding: 20px 20px;
+    border: 1px solid rgb(161, 161, 161);
+}
+</style>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img{
+    width: 20vh;
+    height: auto;
+}
+
+p {
+    margin: 0 40px;
+}
+
+button{
+    width: 50px;
+    height: 30px;
+    border: none;
+    margin: 0 5px;
+}
+
+button:first-of-type{
+    background-color: rgb(212, 212, 212);
+}
+
+button:first-of-type:hover{
+    background-color: rgb(134, 134, 134);
+}
+
+button:last-of-type{
+    background-color: rgb(221, 0, 0);
+}
+
+button:last-of-type:hover{
+    background-color: rgb(255, 0, 0);
+}
+</style>
---
+++ src/components/CreateArticle.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="create-article">
+    <h2>Créer une nouvelle page du blog</h2>
+    <div>
+        <label for="">Titre de la page</label>
+        <input type="text" v-model="title">
+        <label for="">Meta Title</label>
+        <input type="text" v-model="metaTitle">
+        <label for="">Meta Description</label>
+        <input type="text" v-model="metaDescription">
+    </div>
+    <div>
+        <img :src="img" alt="#">
+        <p>Ajouter une image</p>
+    </div>
+    <div>
+        <label for="">Corps du post</label>
+        <input type="text" v-model="description">
+    </div>
+    <router-link to="/admin" id="admin-button-create" @click="sendArticle([title, metaTitle, metaDescription, description])">Create Article</router-link>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+    name: 'create-article',
+    computed: {
+        ...mapState(['img', 'text'])
+    },
+    methods: {
+        sendArticle: function(article) {
+            this.$emit('newArticle', article)
+        }
+    }
+}
+</script>
+
+<style>
+#create-article {
+    display: flex;
+    flex-wrap: wrap;
+}
+
+#create-article div:first-of-type {
+    display: flex;
+    flex-direction: column;
+    width: 60%;
+}
+
+#create-article div:nth-of-type(2) {
+    width: 40%;
+    text-align: center;
+}
+
+#create-article div:nth-of-type(2) img {
+    width: 10vw;
+    height: auto;
+    border-radius: 50px;
+}
+
+#create-article div:last-of-type {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+}
+
+#create-article div:last-of-type input {
+    width: 40%;
+    height: 20vh;
+    margin: 0 3vw;
+}
+
+h2 {
+    width: 100%;
+    text-align: center;
+}
+</style>
