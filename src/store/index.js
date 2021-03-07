74aa25eb4f4c5dd98b9e760037569372516aa29c
Claire <claire.brisbart@edu.devinci.fr>
2021-03-07 22:45:45 +0100

    ADD - store with vuex

---
+++ src/store/index.js
@@ -0,0 +1,14 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    img: "https://journalmetro.com/wp-content/uploads/2017/04/default_profile_400x400.png?w=860",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis exercitationem, esse cum cupiditate voluptatem ut, unde omnis nihil, ea id facilis molestiae ipsam? Perferendis explicabo unde voluptate! Voluptatibus, rem iure."
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
