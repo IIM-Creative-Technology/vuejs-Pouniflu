40a3503072f13a26cbb9f057f9ae3dcc7a24849e
Claire <claire.brisbart@edu.devinci.fr>
2021-03-07 22:46:24 +0100

    ADD - views Admin, Blog, Home

---
+++ src/views/Admin.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="admin">
+        <div v-if="$route.name != 'CreateArticle'">
+            <router-link to="/create-an-article" id="admin-button-article">Create Article</router-link>
+            <ArticleList />
+        </div>
+        <div v-else>
+            <CreateArticle />
+        </div>
+    </div>
+</template>
+
+<script>
+import ArticleList from '@/components/ArticleList.vue'
+import CreateArticle from '@/components/CreateArticle.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        ArticleList,
+        CreateArticle
+    }
+}
+</script>
+
+<style>
+#admin-button-article, #admin-button-create {
+    padding: 10px 16px;
+    background: rgb(22, 84, 255);
+    color: rgb(255, 255, 255);
+    border: none;
+    border-radius: 2px;
+    text-decoration:none;
+}
+
+#admin-button-article:hover, #admin-button-create:hover{
+    background: rgb(11, 46, 141);
+}
+
+#admin-button-article {
+    position: absolute;
+    top: 4vh;
+    right: 9.2vw;
+}
+
+#admin-button-create {
+    position: relative;
+    left: 70vw;
+    top: 6vh;
+}
+</style>
