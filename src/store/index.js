237f37efbe8190510b7755b23f957496afcc07ea
Claire <claire.brisbart@edu.devinci.fr>
2021-03-15 09:33:06 +0100

    ADD - 1 post for example

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,14 @@ export default createStore({
   state: {
     img: "https://journalmetro.com/wp-content/uploads/2017/04/default_profile_400x400.png?w=860",
     text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis exercitationem, esse cum cupiditate voluptatem ut, unde omnis nihil, ea id facilis molestiae ipsam? Perferendis explicabo unde voluptate! Voluptatibus, rem iure.",
-    articles: []
+    articles: [
+      {
+        title: "Le Royaume des chats", 
+        metaTitle: "Film Ghibli", 
+        metaDescription: "Ce film est un film Ghibli", 
+        description: "Haru est une jeune lycéenne de 17 ans pleine de doutes qui ne trouve pas sa place au milieu des autres. Sa vie bascule le jour où, sur le chemin du retour du lycée avec sa meilleure amie Hiromi, elle sauve la vie d'un chat qui manque d'être écrasé par un camion."
+      }
+    ]
   },
   mutations: {
     CREATE_ARTICLE(state, payload) {
